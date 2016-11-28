04450f37f41ce3932acac1db330ed9be5d116a22
Dao Hoang Tung <tung.coder1992@gmail.com>
2016-11-28 17:55:17 +0700

    Update

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -15,20 +15,18 @@ using namespace std;
 class HelloWorld : public cocos2d::Layer
 {
 public:
-    
-    Mission* mission;
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
 
-    // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
     
-    virtual void onEnterTransitionDidFinish();
-    
+private:
+    Mission* mission;
+
     void update(float dt);
+    
+    virtual void onEnterTransitionDidFinish();
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
